93cd97dc80695c1a35cfbe5d62312d13a189a64e
felipe alferez <alferezfelipe@gmail.com>
2024-02-10 03:16:27 -0500

    add zoom page

--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {Map as MapBox} from 'mapbox-gl'
 @Component({
   templateUrl: './zoom-page.component.html',
   styleUrls: ['./zoom-page.component.css']
 })
-export class ZoomPageComponent {
+export class ZoomPageComponent implements AfterViewInit {
 
+  @ViewChild('map') containerMap? : ElementRef
+
+  ngAfterViewInit(): void {
+
+    if(!this.containerMap) throw 'no';
+    console.log(this.containerMap)
+
+    const map = new MapBox({
+      container: this.containerMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    })
+    
+  }
 }
