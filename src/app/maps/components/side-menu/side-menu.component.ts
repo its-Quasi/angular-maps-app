2cde992da26de27eeec5d4b023eaf5dce4ae7f88
felipe alferez <alferezfelipe@gmail.com>
2024-02-09 19:57:51 -0500

    add side menu

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name : string,
+  route : string
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'map-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
-
+  menuItems : MenuItem[] = [
+    {name : 'Fullscreen' , route : '/maps/fullscreen'},
+    {name : 'Zoom' , route : '/maps/zoom'},
+    {name : 'Markers' , route : '/maps/markers'},
+    {name : 'Houses' , route : '/maps/properties'},
+  ]
 }
--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { ZoomPageComponent } from './pages/zoom-page/zoom-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 
 const routes: Routes = [
   {
@@ -9,9 +12,9 @@ const routes: Routes = [
     component : MapsLayoutComponent,
     children : [
       {path : 'fullscreen' , component : FullScreenPageComponent},
-      {path : 'zoom' , component : FullScreenPageComponent},
-      {path : 'markers' , component : FullScreenPageComponent},
-      {path : 'properties' , component : FullScreenPageComponent},
+      {path : 'zoom' , component : ZoomPageComponent},
+      {path : 'markers' , component : MarkersPageComponent},
+      {path : 'properties' , component : PropertiesPageComponent},
       {path : '**' , redirectTo : 'fullscreen'},
     ]
   }
