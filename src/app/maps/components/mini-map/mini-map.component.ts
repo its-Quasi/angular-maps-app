56de80172c81b8797010f62a3bfe57fcebd95ee4
felipe alferez <alferezfelipe@gmail.com>
2024-02-15 17:02:42 -0500

    finish projec

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map as MapBox, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
 
+  @Input() lngLat?: [number, number]
+  @ViewChild('map') containerMap?: ElementRef
+  map?: MapBox
+
+  ngAfterViewInit(): void {
+    if (!this.lngLat) throw 'lnglat cant be null'
+    if (!this.containerMap) throw 'map has been not initialized';
+    this.map = new MapBox({
+      container: this.containerMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive : false
+    })
+    new Marker().setLngLat(this.lngLat).addTo(this.map)
+  }
 }
